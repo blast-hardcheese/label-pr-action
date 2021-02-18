fe70f9bdb04f6c95fcc9b6a3252c81e9e8285cfd
Devon Stewart <dstewart@twilio.com>
2021-02-18 15:58:40 -0800

    Initial test action

---
+++ index.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const filters = core.getInput('filters');
+  console.log(`Filters: ${filters}`);
+  github.issues.addLabels({
+    issue_number: github.context.issue.number,
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    labels: ['chore']
+  })
+} catch (error) {
+  core.setFailed(error.message);
+}
